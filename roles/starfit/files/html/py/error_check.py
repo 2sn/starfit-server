db940d2525db997b593ccb0be40d6ebc047f0479
Conrad Chan <conradchan@hotmail.com.au>
2022-09-27 16:41:23 +1000

    separate error checking, use email_validator package

---
+++ roles/starfit/files/html/py/error_check.py
@@ -0,0 +1,36 @@
+from email_validator import EmailNotValidError, validate_email
+from starfit.dbtrim import TrimDB as StarDB
+from starfit.read import Star
+
+
+def check(filename, dbpath, sol_size, pop_size, plotformat, mail, email):
+    error = []
+    try:
+        Star(filename)
+    except:
+        error += ["There is something wrong with this stellar data."]
+
+    try:
+        StarDB(dbpath)
+    except:
+        error += ["There is something wrong with this database."]
+
+    # Test if the input parameters are any good
+    if sol_size > 10:
+        error += ["Gene sizes greater than 10 are not supported."]
+
+    if pop_size > 1000:
+        error += ["Population sizes over 1000 are not supported."]
+
+    if mail:
+        try:
+            # Check that the email address is valid.
+            validate_email(email, check_deliverability=True)
+
+        except EmailNotValidError:
+            error += ["{} is not a valid email.".format(email)]
+
+    if plotformat == "pdf" and not mail:
+        error += ["PDF plot format must be emailed."]
+
+    return error
