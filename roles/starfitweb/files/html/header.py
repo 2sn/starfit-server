9c0d85e3920084f6b05a2a3c572f23ab9e418c29
Conrad Chan <conradchan@hotmail.com.au>
2022-10-18 11:01:59 +1100

    move CSP into the actual HTTP header rather than using meta tags

--- roles/starfitweb/files/html/header.py
+++ roles/starfitweb/files/html/header.py
@@ -1,4 +1,7 @@
 def http():
     print("Content-Type: text/html; charset=UTF-8")
     print('Cache-Control: "no-cache, no-store, must-revalidate"')
+    print(
+        "Content-Security-Policy: default-src https:; object-src 'self' data:; img-src 'self' data:"
+    )
     print()
