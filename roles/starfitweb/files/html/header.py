30098bbb2e5845d90b0b8aa98ac872e7fce06245
Conrad Chan <conradchan@hotmail.com.au>
2022-10-18 11:16:30 +1100

    Update CSP header
    - allow embedded SVGs
    - explicitly allow google fonts

--- roles/starfitweb/files/html/header.py
+++ roles/starfitweb/files/html/header.py
@@ -2,6 +2,6 @@ def http():
     print("Content-Type: text/html; charset=UTF-8")
     print('Cache-Control: "no-cache, no-store, must-revalidate"')
     print(
-        "Content-Security-Policy: default-src https:; object-src 'self' data:; img-src 'self' data:"
+        "Content-Security-Policy: default-src https: data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;"
     )
     print()
