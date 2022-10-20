5d664b2897874d88fb10af854cefadbf3a54e79f
Conrad Chan <conradchan@hotmail.com.au>
2022-10-20 12:49:54 +1100

    reset loading popup after clicking back

--- roles/starfitweb/files/html/scripts.js
+++ roles/starfitweb/files/html/scripts.js
@@ -14,16 +14,24 @@ function buttonCheck() {
 }
 
 $(document).ready(function () {
+  // On page loads:
+
+  // Run the button check after page finishes loading
+  // This ensures a consistent state after clicking the back button
+  buttonCheck();
+
+  // Explicitly reset the loading popup after pressing back (needed for Firefox)
+  $(window).bind("pageshow", function(event) {
+    $("#processing").css({ "display": "none" });
+  });
+
+  // On actions:
 
   // Detect clicks on the algorithm radio buttons
   const algoButtons = document.getElementsByClassName('algoButton');
   for (let i = 0; i < algoButtons.length; i++) {
     algoButtons[i].addEventListener("click", buttonCheck)
   }
-
-  // Run the button check after page finishes loading
-  // This ensures a consistent state after clicking the back button
-  buttonCheck();
 
   // Show loading popup when the "run" button has been clicked
   $("#run").click(function () {
